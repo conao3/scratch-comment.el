43902ae49a3215cee192d3683f4e46c764a32407
conao3 <conao3@gmail.com>
2020-05-05 18:04:54 +0900

    require Emacs-26.1

--- scratch-comment.el
+++ scratch-comment.el
@@ -5,7 +5,7 @@
 ;; Author: Naoya Yamashita <conao3@gmail.com>
 ;; Version: 0.0.1
 ;; Keywords: convenience
-;; Package-Requires: ((emacs "25.1"))
+;; Package-Requires: ((emacs "26.1"))
 ;; URL: https://github.com/conao3/scratch-comment.el
 
 ;; This program is free software: you can redistribute it and/or modify
